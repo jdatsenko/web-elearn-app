e15e075eb43c653698678856e58d2cb009df58c3
Yuliia Datsenko <xdatsenko@stuba.sk>
2024-04-10 14:32:38 +0200

    myacc fronend fixed

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -4,13 +4,21 @@ import { useEffect, useState } from "react";
 import { SolvedTest } from "@prisma/client";
 import { useSession } from "next-auth/react";
 import { Progress } from "@/components/ui/progress";
+import { Separator } from "@/components/ui/separator";
+
+const topics = [
+  "LPWAN: technológie a aplikácie",
+  "LoRaWAN: Lora Alliance, obmedzenia a výhody",
+  "Architektúra LoRaWAN",
+  "NB-IoT: špecifikácie a výhody",
+  "NB-IoT: aplikácie",
+  "Porovnanie technológií LPWAN",
+];
 
 const Admin = () => {
   const [solvedTests, setSolvedTests] = useState<SolvedTest[]>([]);
   const { data: session, status } = useSession();
   const [progress, setProgress] = useState<number>(0);
-
-
 
   useEffect(() => {
     axios.get("/api/user/test/solved").then((response) => {
@@ -34,14 +42,14 @@ const Admin = () => {
   useEffect(() => {
     const totalTopics = Object.values(groupTestsByTopic()).length;
     let completedTopics = 0;
-  
+
     Object.values(groupTestsByTopic()).forEach((tests) => {
       const topicCompleted = tests.every((test) => test.score === 100);
       if (topicCompleted) {
         completedTopics++;
       }
     });
-  
+
     const newProgress = (completedTopics / totalTopics) * 100;
     setProgress(newProgress);
   }, [solvedTests]);
@@ -55,15 +63,29 @@ const Admin = () => {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Vyriešené témy</h1>
+      <div className="text-center">
+        <h2 className="text-4xl font-bold my-9">
+          {" "}
+          Váš priebeh štúdia, {session?.user?.name}
+        </h2>
+      </div>
+
+      <h1 className="text-2xl font-bold mb-4">
+        Vyriešené témy: {Object.values(groupTestsByTopic()).length}
+      </h1>
       <div className="mb-5">
         <Progress value={progress} />
       </div>
+
+      <Separator />
       {Object.entries(groupTestsByTopic()).map(([topic, tests]) => {
-        let i = 1; 
+        let i = 1;
+        const topicIndex = parseInt(topic, 10) - 1;
         return (
           <div key={topic}>
-            <h2 className="text-lg font-bold my-2">Téma {topic}</h2>
+            <h2 className="text-lg font-bold mt-8 ">
+              {topicIndex+1}. {topics[topicIndex]}
+            </h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
               {tests.map((test, index) => (
                 <div key={index} className="border mb-6 rounded-lg p-4">

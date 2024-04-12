21c37f592d0b71b4f9ea5aca9ba92848edfc92e4
Yuliia Datsenko <xdatsenko@stuba.sk>
2024-04-12 21:14:53 +0200

    frontend for my acc

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -73,18 +73,36 @@ const Admin = () => {
       {Object.entries(groupTestsByTopic()).map(([topic, tests]) => {
         let i = 1;
         const topicIndex = parseInt(topic, 10) - 1;
+        let isTopicSolved = false; 
         return (
           <div key={topic}>
-            <h2 className="text-lg font-bold mt-8 ">
-              {topicIndex+1}. {topics[topicIndex]}
+            <h2 className="text-lg font-bold mt-8 mb-3">
+              {topicIndex + 1}. {topics[topicIndex]}
             </h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              {tests.map((test, index) => (
-                <div key={index} className="border mb-6 rounded-lg p-4">
-                  <p className="mb-2">Pokus: {i++}</p>{" "}
-                  <p className="mb-2">Úspech: {test.score} %</p>
-                </div>
-              ))}
+              {tests.map((test, index) => {
+                if (!isTopicSolved) {
+                    if (test.score === 100) {
+                      isTopicSolved = true;
+                      return (
+                        <div key={index} className="border mb-6 rounded-lg p-4 bg-green-600">
+                          <p className="mb-2">Pokus: {i++}</p>{" "}
+                          <p className="mb-2">Úspech: {test.score} %</p>
+                          <p className="text-green-900 font-bold">Topic Completed!</p>
+                        </div>
+                      );
+                    } else {
+                      return (
+                        <div key={index} className="border mb-6 rounded-lg p-4">
+                          <p className="mb-2">Pokus: {i++}</p>{" "}
+                          <p className="mb-2">Úspech: {test.score} %</p>
+                          <Progress value={test.score} />
+                        </div>
+                      );
+                    }
+                  }
+                  return null; 
+              })}
             </div>
           </div>
         );
